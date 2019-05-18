05ae03adfa34efa62a1aa6c12b00d0aa48625695
Ricky White <ricky@whitelionmedia.com>
2019-05-18 07:49:15 -0400

    Painted Black. Also changed file path of JSON putput

--- scrape.py
+++ scrape.py
@@ -3,22 +3,23 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 def realpython():
-    url = 'https://realpython.com/team/rwhite/'
+    url = "https://realpython.com/team/rwhite/"
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, "html.parser")
 
-    h2 = soup.find('h2')
-    ul = h2.find_next('ul')
+    h2 = soup.find("h2")
+    ul = h2.find_next("ul")
 
-    with open('real_python_tuts.json', 'w') as tuts:
+    with open("json_data/real_python_tuts.json", "w") as tuts:
 
         tutorial_list = []
-        for item in ul.find_all('li'):
-            link = 'https://realpython.com' + item.a['href']
+        for item in ul.find_all("li"):
+            link = "https://realpython.com" + item.a["href"]
             title = item.text
 
-            item_dict = { "title": title, "link": link }
+            item_dict = {"title": title, "link": link}
             tutorial_list.append(item_dict)
 
         json.dump(tutorial_list, tuts)
