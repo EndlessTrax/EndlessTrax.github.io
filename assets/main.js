060727f1b6612eba1fdaa398b7ee507093505336
Ricky White <ricky@whitelionmedia.com>
2019-05-18 08:28:07 -0400

    Added javascript to append articles from JSON file

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,14 @@
-// 
+//
 // JavaScript File for Ricky White .dev
-// 
+//
 
-const AuthorPageURL = "https://realpython.com/team/rwhite/";
+const realPythonJsonPath = "json_data/real_python_tuts.json";
+const realPythonArticleDiv = document.getElementById("real-python-articles");
+
+$.getJSON(realPythonJsonPath, function(data) {
+  data.forEach(element => {
+    let item = document.createElement("li");
+    item.innerHTML = '<a href="' + element.link + '">' + element.title + "</a>";
+    realPythonArticleDiv.appendChild(item);
+  });
+});
